14922d816183c32cdbebc07381d4c178a8f154aa
juan sava <juan sava>
2016-12-21 22:01:40 -0300

    update service

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,11 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.service('Todos', function Todos() {
+
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
+  
+    this.data = [{
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
@@ -19,32 +20,22 @@ angular.module('starter.services', [])
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
   }];
+ 
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+    this.all = function all (){
+      return this.data
     }
-  };
+    this.remove = function remove (todo){
+      this.data.splice (this.data.indexOf(chat), 1)
+    }
+    this.get = function get (id) {
+      var todo = {}
+      this.data.map (function(el){
+        if (el.id == id){
+          todo = el
+        }
+      })
+    }
+    return todo
 });
